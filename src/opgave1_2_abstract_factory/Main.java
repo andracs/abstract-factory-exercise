722b7c071a9e332168d3a9645b495e75e38b3c48
Andras Acs <acsandras@gmail.com>
2017-02-28 23:12:38 +0100

    Final adjustments, comments

--- src/opgave1_2_abstract_factory/Main.java
+++ src/opgave1_2_abstract_factory/Main.java
@@ -15,22 +15,30 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
+        System.out.println("***************************************");
+        World middleEarth = World.getWorldInstance();
+        System.out.println("The name of our world is " + middleEarth.getName() + "!");
+        System.out.println("***************************************");
+        
+        // Creating facories
         InfantryFactory humanFactory = FactoryFactory.createInfantryFactory(1);
         InfantryFactory orcFactory = FactoryFactory.createInfantryFactory(2);
 
+        // The factories are producing units        
         InfantryUnit rh = humanFactory.createUnit(1); // unit 1 -- rockthrower
         rh.setName("Durotan");
         InfantryUnit st = orcFactory.createUnit(2); // unit 2 -- spearthrower
         st.setName("Orgrim");
         ;
 
-        // Fighting for death
+        // The units are fighting for death
         while (rh.alive() && st.alive()) {
             rh.attack(st);
             st.attack(rh);
         }
-
+        
+        // The battle reults are announced
         System.out.println("***************************************");
         if (!rh.alive()) {
             if (!st.alive()) {
---
+++ src/opgave1_2_abstract_factory/World.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opgave1_2_abstract_factory;
+
+/**
+ *
+ * @author andrasacs
+ */
+
+// Implementing the singleton pattern
+// Only onw world can be created, and that is middleEarth
+public class World {
+    private static World middleEarth;    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+            
+    private World() {}
+   
+    public static World getWorldInstance() {
+        if (middleEarth instanceof World) {
+            // Do nothing
+        } else {
+            middleEarth = new World();
+        }
+        middleEarth.name = "Middle Earth"; 
+        return middleEarth;
+    }
+}
